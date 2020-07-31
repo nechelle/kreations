219b6f025c4503f46852bdab6fdd0d2c3ffb00fd
Nechelle Burns <nburns1@gmail.com>
2020-07-30 22:07:44 -0400

    Added Bootstrap to forms

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function ShowNav() {
-  let navItems = document.querySelectorAll('ul > li');
- for(let i = 0; i < navItems.length; i++) {
-    navItems[i].classList.toggle('nav');
+  let navMenu = document.querySelectorAll('ul > li');
+ for(let i = 0; i < navMenu.length; i++) {
+    navMenu[i].classList.toggle('navigation');
   }
 }
 //var visible = false;
 //function ShowNav() {
   //visible = !visible;
-  //let navItems = document.querySelectorAll('ul > li');
-  //for (let i = 0; i < navItems.length; i++) {
-   // navItems[i].style.display = visible ? 'inherit' : 'none';
+  //let navMenu = document.querySelectorAll('ul > li');
+  //for (let i = 0; i < navMenu.length; i++) {
+   // navMenu[i].style.display = visible ? 'inherit' : 'none';
   //}
 //}
